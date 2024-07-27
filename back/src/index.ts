c5aceb7e1ae702e508bd7202e278341607505569
Alexandre Vernet <alexandre.vernet99@gmail.com>
2024-07-27 18:13:56 +0200

    feat(env): Add .env

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 import router from "./router/router";
 import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
 app.use(express.json());
 app.use('/api', router);
 
